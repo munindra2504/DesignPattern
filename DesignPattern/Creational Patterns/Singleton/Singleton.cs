6c789ad1142577e53e2fb70d7db1afd4c065550b
Munindra M Gupta <munindra_rbs@hotmail.com>
2018-03-19 07:47:50 -0700

    Singletone Design Pattern

---
+++ DesignPattern/Creational Patterns/Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational_Patterns.Singleton
+{
+    public class Singleton
+    {
+        private int counter = 0;
+        private static readonly Singleton _instance;
+
+        private static object obj;
+        public static Singleton Instance {
+            get
+            {
+                if (_instance == null)
+                    return new Singleton();
+
+                return _instance;
+            }
+        }
+
+        static Singleton()
+        {
+            if(_instance == null)
+            {
+                lock(obj){
+                    if (_instance == null)
+                        _instance = new Singleton();
+                }
+            }
+        }
+
+        public int GetCounter()
+        {
+            return counter++;
+        }
+    }
+}
